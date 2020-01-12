209ebaa132deaf8291ad0d05184f9c1cd8dca85c
kovax <kovacsdanielhun@gmail.com>
2020-01-12 17:00:37 +0100

    Fixed topic

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
-    'Topic :: Monitoring',
+    'Topic :: System :: Monitoring',
     'License :: OSI Approved :: GNU General Public License v3.0',
     'Programming Language :: Python :: 3',
   ],
