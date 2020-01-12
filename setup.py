46b6ed913991e2a09f14e5c25fc668c60908194d
kovax <kovacsdanielhun@gmail.com>
2020-01-12 19:11:10 +0100

    Added ZBXNotifier.py script

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setup(
         'appdirs',
         'configparser'
       ],
-  classifiers=[
+  scripts = ['scripts/ZBXNotifier.py'],
+  classifiers =[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
     'Topic :: System :: Monitoring',
